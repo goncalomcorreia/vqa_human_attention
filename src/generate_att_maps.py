f3928fd1dcb88fca9be31180a370c992e43f8bf9
goncalomcorreia <goncalommac@gmail.com>
2017-06-05 16:18:20 +0100

    fix path

--- src/generate_att_maps.py
+++ src/generate_att_maps.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import sys
-sys.path.append('/home/s1670404/imageqa-san/src/')
+sys.path.append('/home/s1670404/vqa_human_attention/src/')
 
 from optimization_weight import *
 from san_att_conv_twolayer_theano import *
@@ -8,7 +8,7 @@ from data_provision_att_vqa import *
 from data_processing_vqa import *
 
 import pickle
-f = open('/home/s1670404/imageqa-san/data_vqa/answer_dict.pkl', 'r')
+f = open('/home/s1670404/vqa_human_attention/data_vqa/answer_dict.pkl', 'r')
 answer_dict = pickle.load(f)
 f.close()
 answer_dict = {v: k for k, v in answer_dict.iteritems()}
@@ -16,7 +16,7 @@ answer_dict = {v: k for k, v in answer_dict.iteritems()}
 result = OrderedDict()
 
 options, params, shared_params = load_model(
-    '/home/s1670404/imageqa-san/expt/imageqa_best_0.523.model')
+    '/home/s1670404/vqa_human_attention/expt/imageqa_best_0.523.model')
 
 image_feat, input_idx, input_mask, label, \
 dropout, cost, accu, pred_label, \
