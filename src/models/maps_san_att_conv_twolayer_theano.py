fb9e1f3b1050d38420ea92cc1b02fde85dfe2a63
Gonçalo Correia <goncalocorreia@Goncalos-MacBook-Pro.local>
2017-07-06 08:10:47 +0100

    bugfix

--- src/models/maps_san_att_conv_twolayer_theano.py
+++ src/models/maps_san_att_conv_twolayer_theano.py
@@ -172,11 +172,19 @@ def init_shared_params_maps(shared_params, options):
     shared_params_maps = OrderedDict()
     for k, p in shared_params.iteritems():
         if options['maps_second_att_layer']:
-            if 'combined_mlp' not in k:
-                shared_params_maps[k] = shared_params[k]
+            if options['use_third_att_layer']:
+                if 'combined_mlp' not in k and 'mlp_3' not in k:
+                    shared_params_maps[k] = shared_params[k]
+            else:
+                if 'combined_mlp' not in k:
+                    shared_params_maps[k] = shared_params[k]
         else:
-            if 'combined_mlp' not in k and 'mlp_2' not in k:
-                shared_params_maps[k] = shared_params[k]
+            if options['use_third_att_layer']:
+                if 'combined_mlp' not in k and 'mlp_2' not in k and 'mlp_3' not in k:
+                    shared_params_maps[k] = shared_params[k]
+            else:
+                if 'combined_mlp' not in k and 'mlp_2' not in k:
+                    shared_params_maps[k] = shared_params[k]
 
     return shared_params_maps
 
