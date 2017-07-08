538e09198a277c9849d026f74fea169e4148ec72
Gonçalo Correia <goncalocorreia@Goncalos-MacBook-Pro.local>
2017-07-08 18:48:36 +0100

    debug

--- src/scripts/tuning/hsan_tuning_and_plotting.py
+++ src/scripts/tuning/hsan_tuning_and_plotting.py
@@ -254,12 +254,10 @@ def train(options):
                 batch_image_feat = reshape_image_feat(batch_image_feat,
                                                       options['num_region'],
                                                       options['region_dim'])
-                try:
-                    [map_cost_val] = f_val_subtask(batch_image_feat, np.transpose(input_idx),
-                                         np.transpose(input_mask),
-                                         batch_map_label)
-                except:
-                    import pdb; pdb.set_trace()
+                import pdb; pdb.set_trace()
+                [map_cost_val] = f_val_subtask(batch_image_feat, np.transpose(input_idx),
+                                     np.transpose(input_mask),
+                                     batch_map_label)
                 val_count += batch_image_feat.shape[0]
                 val_map_cost_list.append(map_cost_val * batch_image_feat.shape[0])
 
