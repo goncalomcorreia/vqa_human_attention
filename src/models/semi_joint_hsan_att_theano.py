96944521b5fe0bcb26999964b5d7f950c7ee019d
Gonçalo Correia <goncalocorreia@Goncalos-MacBook-Pro.local>
2017-07-08 18:15:23 +0100

    bugfix

--- src/models/semi_joint_hsan_att_theano.py
+++ src/models/semi_joint_hsan_att_theano.py
@@ -354,7 +354,7 @@ def build_model(shared_params, options):
     prob_attention_2 = T.nnet.softmax(combined_feat_attention_2[:, :, 0])
 
     if prob_attention_2.shape[0] != map_label.shape[0]:
-        prob_attention_2_section = prob_attention_2[:int(options['hat_frac']*prob_attention_2.shape[0])]
+        prob_attention_2_section = prob_attention_2[:int(options['hat_frac']*options['batch_size'])]
     else:
         prob_attention_2_section = prob_attention_2
 
