4c5eabbc20a6898334cdd5cb5689241c27c6bf75
goncalomcorreia <s1670404@shriver.inf.ed.ac.uk>
2017-08-16 18:23:53 +0100

    Renamed and moved things around

--- script.sh
+++
@@ -1,2 +0,0 @@
-nice python vqa_human_attention/src/scripts/hsan_deepfix.py --changes "'model_name'":"'no_supervision_baseline'" "'lambda'":"0."
-nice python vqa_human_attention/src/scripts/hsan_deepfix.py --changes "'model_name'":"'hsan_deepfix'" "'lambda'":"0.2"
--- script2.sh
+++
@@ -1,2 +0,0 @@
-nice python vqa_human_attention/src/scripts/hsan_no_deepfix.py
-nice python vqa_human_attention/src/scripts/hsan_deepfix_par.py
